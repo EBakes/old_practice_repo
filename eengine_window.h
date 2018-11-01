9237a7f9fa9a5fd3fce68064deacde303e102d95
Erik Baker <erik.baker@digipen.edu>
2018-11-01 11:53:19 -0700

    added base code

---
+++ eengine_window.h
@@ -0,0 +1,24 @@
+// ---------------------------------------------------------------------
+// @author Erik Baker
+// @date   11/1/18
+// @file   eengine_window.h
+// @brief  contains eengine_window implementation
+// ---------------------------------------------------------------------
+#ifndef EENGINE_WINDOW_H
+#define EENGINE_WINDOW_H
+
+template <unsigned engine_index>
+class eengine_window
+{
+	public:
+	
+	eengine_window()
+	{
+		
+	}
+	
+	private:
+	
+}
+
+#endif
---
+++ template_array.h
@@ -0,0 +1,10 @@
+#ifndef TEMPLATE_ARRAY_H
+#define TEMPLATE_ARRAY_H
+
+template <typename T, unsigned i = 0>
+struct template_array
+{
+	static T data;
+};
+
+#endif
